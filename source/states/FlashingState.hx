64978c5659524c713a0f881c7404952d47f16049
Yugisgiy <hetgegeg@gmail.com>
2025-03-08 17:54:39 +0300

    Add files via upload

--- source/states/FlashingState.hx
+++ source/states/FlashingState.hx
@@ -3,99 +3,49 @@ package states;
 import flixel.FlxSubState;
 
 import flixel.effects.FlxFlicker;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
 import lime.app.Application;
 
 class FlashingState extends MusicBeatState
 {
 	public static var leftState:Bool = false;
+	var warnSprite:FlxSprite;
 
-	var isYes:Bool = true;
-	var texts:FlxTypedSpriteGroup<FlxText>;
-	var bg:FlxSprite;
 
 	override function create()
 	{
 		super.create();
 
-		final enter:String = (controls.mobileC) ? 'A' : 'ENTER';
-		final back:String = (controls.mobileC) ? 'B' : 'BACK';
+		warnSprite = new FlxSprite();
+        warnSprite.loadGraphic(Paths.image('warn'));
+		warnSprite.scale.set(0.666666, 0.666666);
+		warnSprite.updateHitbox();
+		warnSprite.antialiasing = true;
+        warnSprite.alpha = 0;
+        add(warnSprite);
 
-		bg = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
-		add(bg);
-
-		texts = new FlxTypedSpriteGroup<FlxText>();
-		texts.alpha = 0.0;
-		add(texts);
-
-		var warnText:FlxText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
-			Do you wish to disable them?");
-		warnText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER);
-		warnText.screenCenter(Y);
-		texts.add(warnText);
-
-		final keys = ["Yes", "No"];
-		for (i in 0...keys.length) {
-			final button = new FlxText(0, 0, FlxG.width, keys[i]);
-			button.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER);
-			button.y = (warnText.y + warnText.height) + 24;
-			button.x += (128 * i) - 80;
-			texts.add(button);
-		}
-
-		addTouchPad("LEFT_RIGHT", "A_B");
-		touchPad.alpha = 0;
-
-		FlxTween.tween(texts, {alpha: 1.0}, 0.5, {
-			onComplete: (_) -> updateItems()
-		});
-		FlxTween.tween(touchPad, {alpha: 1.0}, 0.5);
+		FlxTween.tween(warnSprite, { alpha: 1 }, 1, {
+            onComplete: function(twn: FlxTween) {
+                warnSprite.alpha = 1;
+            }
+        });
 	}
 
 	override function update(elapsed:Float)
 	{
-		if(leftState) {
-			super.update(elapsed);
-			return;
-		}
-		var back:Bool = controls.BACK;
-		if (controls.UI_LEFT_P || controls.UI_RIGHT_P) {
-			FlxG.sound.play(Paths.sound("scrollMenu"), 0.7);
-			isYes = !isYes;
-			updateItems();
-		}
-		if (controls.ACCEPT || back) {
-			leftState = true;
-			FlxTransitionableState.skipNextTransIn = true;
-			FlxTransitionableState.skipNextTransOut = true;
-			if(!back) {
-				ClientPrefs.data.flashing = !isYes;
-				ClientPrefs.saveSettings();
-				FlxG.sound.play(Paths.sound('confirmMenu'));
-				final button = texts.members[isYes ? 1 : 2];
-				FlxFlicker.flicker(button, 1, 0.1, false, true, function(flk:FlxFlicker) {
-					new FlxTimer().start(0.5, function (tmr:FlxTimer) {
-						FlxTween.tween(texts, {alpha: 0}, 0.2, {
-							onComplete: (_) -> MusicBeatState.switchState(new TitleState())
-						});
-						FlxTween.tween(touchPad, {alpha: 0}, 0.2);
-					});
+		if(!leftState) {
+			if (controls.ACCEPT || controls.BACK) {
+				leftState = true;
+				FlxTransitionableState.skipNextTransIn = true;
+				FlxTransitionableState.skipNextTransOut = true;
+				FlxTween.tween(warnSprite, { alpha: 0 }, 1, {
+					onComplete: function(twn: FlxTween) {
+						MusicBeatState.switchState(new TitleState());
+					}
 				});
-			} else {
-				FlxG.sound.play(Paths.sound('cancelMenu'));
-				FlxTween.tween(texts, {alpha: 0}, 1, {
-					onComplete: (_) -> MusicBeatState.switchState(new TitleState())
-				});
-				FlxTween.tween(touchPad, {alpha: 0}, 1);
 			}
 		}
 		super.update(elapsed);
-	}
-
-	function updateItems() {
-		// it's clunky but it works.
-		texts.members[1].alpha = isYes ? 1.0 : 0.6;
-		texts.members[2].alpha = isYes ? 0.6 : 1.0;
 	}
 }
